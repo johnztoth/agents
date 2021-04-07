65ecc81d1fd820c72f22d7668ea870acb9f6dbd9
johnztoth <gy20jt@leeds.ac.uk>
2021-04-07 15:30:21 +0530

    Updating comments and webpage

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
 # model.py
+#
 # Agents!
 #
-# Define a number of Agents, each with a random x and y coordinate
-# Cycle through a number of iterations
-# For each iteration randomly move the agent by 1 unit in each direction
-# Calculate the final distance between all the agents
-# Plot the final agent positions using Matplotlib.pyplot
+# Create a number of Agents, each with a random x and y coordinate (0-99).
+# Cycle through a number of iteration steps.
+# For each iteration randomly move the agent by +/- 1 unit in each direction.
+# The agent must stay within the bounds of the model 0-99.
+# If the agent steps below 0 its position is shifted to 99.
+# If the agent steps above 99 its position is shifted to 0.
+# Calculate the final distance between all the agents.
+# Plot the final agent positions using Matplotlib.
+# Highlight the two closest Agents and the two furthest Agents
 
 import operator
 import matplotlib.pyplot
@@ -21,11 +26,11 @@ num_of_agents = 10
 num_of_iterations = 100
 agents = []
 
-# Make the agents
+# Make the agents using the Agent class
 for i in range(num_of_agents):        
     agents.append(agentframework.Agent())
 
-# Move the agents
+# Move the agents using the move method
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
         agents[i].move()
@@ -37,30 +42,26 @@ matplotlib.pyplot.xlim(0, 99)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y,color="blue")
 
-# Distance between all agents (redundant)
-for agents_row_a in agents:
-    for agents_row_b in agents:
-        distance = distance_between(agents_row_a, agents_row_b)
 
 # Distance between all agents stored in a list
 # Do not include repeated pairings or self pairings
 mat=[]
 
-for i in range(num_of_agents-1):            # rows
-    for j in range(i+1,num_of_agents):      # columns
+for i in range(num_of_agents-1):            
+    for j in range(i+1,num_of_agents):      
         distance=distance_between(agents[i], agents[j])
         mat.append([i,j,distance])
 
 # Maximum and Minimum distance between two agents
 dmax = max(mat, key=operator.itemgetter(2))
 dmin = min(mat, key=operator.itemgetter(2))
+print("\nmax distance is ",dmax[2],"between agents",dmax[0],"and",dmax[1])
+print("\nmin distance is ",dmin[2],"between agents",dmin[0],"and",dmin[1])
 
 # Plot maximum and minimum pairs
-print("\nmax distance is ",dmax[2],"between agents",dmax[0],"and",dmax[1])
 matplotlib.pyplot.scatter(agents[dmax[0]].x,agents[dmax[0]].y,color="red")
 matplotlib.pyplot.scatter(agents[dmax[1]].x,agents[dmax[1]].y,color="red")
 
-print("\nmin distance is ",dmin[2],"between agents",dmin[0],"and",dmin[1])
 matplotlib.pyplot.scatter(agents[dmin[0]].x,agents[dmin[0]].y,color="green")
 matplotlib.pyplot.scatter(agents[dmin[1]].x,agents[dmin[1]].y,color="green")
 
